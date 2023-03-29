7668906566a9e1562f1820518d3dcf198848e835
thiagowaib <thiagowaib@gmail.com>
2023-03-28 22:15:09 -0300

    Model Attribute Decorator

--- Models/Funcionario.cs
+++ Models/Funcionario.cs
@@ -5,12 +5,18 @@ namespace BSIStore.Models
     public class Funcionario
     {
         public int      id             {get; set;}
+
+        [Display(Name = "Nome")]
         public string?  nome           {get; set;}
 
+        [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         public DateTime dataNascimento {get; set;}
 
+        [Display(Name = "Cargo")]
         public string?  cargo          {get; set;}
+        
+        [Display(Name = "Salário (R$)")]
         public double   salario        {get; set;}
     }
 }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -5,12 +5,18 @@ namespace BSIStore.Models
     public class Produto
     {
         public int      id           {get; set;}
+
+        [Display(Name = "Descrição")]
         public string?  descricao    {get; set;}
 
+        [Display(Name = "Data de Validade")]
         [DataType(DataType.Date)]
         public DateTime dataValidade {get; set;}
 
+        [Display(Name = "Categoria")]
         public string?  categoria    {get; set;}
+
+        [Display(Name = "Preço (R$)")]
         public double   preco        {get; set;}
     }
 }
